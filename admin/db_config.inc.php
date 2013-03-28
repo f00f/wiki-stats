b4e9be1de9fb98951b6a06a3af1ae35bfe11280b
f00f <f00f@f00f.de>
2013-03-28 09:33:20 +0100

    removed admin/.htaccess and admin/db_config.inc.php from index

--- admin/db_config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* included by sql.inc.php */
-
-## mySQL Datenbank Variablen
-$sql->server    = "YOUR DB SERVER (HOST)";
-$sql->user      = "YOUR DB USERNAME";
-$sql->passwort  = "YOUR DB PASSWORD";
-$sql->datenbank = "YOUR DB's NAME";
